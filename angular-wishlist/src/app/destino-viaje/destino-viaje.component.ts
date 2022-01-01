5bad559141ce675303472bf229ee10042e84f232
Miguel Colon <colonfe@yahoo.com.ar>
2022-01-01 16:06:55 -0600

    mejoras en lista de viajes con clases

--- angular-wishlist/src/app/destino-viaje/destino-viaje.component.ts
+++ angular-wishlist/src/app/destino-viaje/destino-viaje.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { DestinoViaje } from '../models/destino-viaje.model';
 
 @Component({
   selector: 'app-destino-viaje',
@@ -7,7 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DestinoViajeComponent implements OnInit {
 
-	@Input() nombre: string;
+	@Input() destino: DestinoViaje;
+  @HostBinding('attr.class') ccsClass = 'col-md-4';
 	
   constructor() { 
 	
--- angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
+++ angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DestinoViaje } from '../models/destino-viaje.model';
 
 @Component({
   selector: 'app-lista-destinos',
@@ -7,18 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaDestinosComponent implements OnInit {
 
-	destinos: string[];
+	destinos: DestinoViaje[];
 	
   constructor() { 
-	this.destinos = ['Barranquilla','Lima','Buenos Aires','Barcelona'];
+	this.destinos = [];
   }
 
   ngOnInit(): void {
   }
   
   guardar(nombre:string, url:string):boolean {
-	  console.log(nombre);
-	  console.log(url);
+	  this.destinos.push(new DestinoViaje(nombre, url));
+    //console.log(this.destinos);
+	  
 	  return false; //para que recargue la pagina con el post del formulario
   }
 
